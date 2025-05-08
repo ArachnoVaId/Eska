6ea08ace4960ca0c9700b85d5952a3f764bd0b1f
hilmiwismadi <dzakiwismadi@gmail.com>
2025-05-08 19:28:03 +0700

    feat: return homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,19 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import AboutUs from "@/components/home/aboutus";
+import Hero from "@/components/home/hero";
+import SeriesOfEvents from "@/components/home/series-of-events/series-of-events";
+import OurMediaPartners from "@/components/collab/mediapartners";
+import OurSponsors from "@/components/collab/sponsors";
 
 const Home = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/registration/Presale", { replace: true });
-  }, [navigate]);
-
-  return null;
+  return (
+    <main className="flex flex-col overflow-x-hidden">
+      <Hero />
+      <AboutUs />
+      <SeriesOfEvents />
+      <OurSponsors />
+      <OurMediaPartners variant="home" />
+    </main>
+  );
 };
 
 export default Home;
