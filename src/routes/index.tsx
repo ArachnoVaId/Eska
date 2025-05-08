6f74d37ca0a10bd85322b773d584d3c27809e997
hilmiwismadi <dzakiwismadi@gmail.com>
2025-05-09 00:34:31 +0700

    fix redirect all to main

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "../pages/home";
 import RootLayout from "./root-layout";
+import RedirectHome from "@/pages/redirect";
 
 export default function AppRoutes() {
   return (
@@ -8,7 +9,7 @@ export default function AppRoutes() {
       <Route element={<RootLayout />}>
         <Route path="/" element={<Home />} />
         {/* Redirect any unknown routes to "/" */}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<RedirectHome />} />
       </Route>
     </Routes>
   );
